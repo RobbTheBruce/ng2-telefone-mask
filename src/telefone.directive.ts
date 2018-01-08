e30c3d0bf062269687733ec1fa41031285b36469
roberto <rob-mg@hotmail.com>
2018-01-07 22:01:19 -0200

    primeiro commit

---
+++ src/telefone.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, ElementRef, HostListener, Renderer } from '@angular/core';
+
+@Directive({
+    selector: '[TelefoneMask]'
+})
+export class TelefoneMask {
+    public nativeElement: HTMLInputElement;
+
+    constructor(public element: ElementRef, public render: Renderer) {
+        this.nativeElement = this.element.nativeElement;
+        this.nativeElement.maxLength = 15;
+    }
+
+    @HostListener('keydown', ['$event']) onKeyDown(event) {
+        let e = <KeyboardEvent>event;
+        if ([46, 8, 9, 27, 13].indexOf(e.keyCode) !== -1) {
+            return;
+        }
+        let ch = (e.key);
+        let regEx = new RegExp('^[0-9]*$');
+        if (regEx.test(ch))
+            return;
+        else
+            e.preventDefault();
+
+    }
+
+    @HostListener('keyup', ['$event']) onKeyUp(e: Event) {
+        let v = this.nativeElement.value;
+        v = v.replace(/\D/g, "");
+        v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
+        v = v.replace(/(\d)(\d{4})$/, "$1-$2");
+        this.nativeElement.value = v;
+    }
+
+}
